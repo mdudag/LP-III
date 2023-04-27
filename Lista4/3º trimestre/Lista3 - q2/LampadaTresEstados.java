a9209eb7f5acef5a627303c9c8582503a715bdae
Duda <106630667+mdudag@users.noreply.github.com>
2023-04-26 21:56:50 -0300

    Create LampadaTresEstados.java

---
+++ Lista4/3º trimestre/Lista3 - q2/LampadaTresEstados.java
@@ -0,0 +1,117 @@
+public class LampadaTresEstados {
+  private String tipoDaLamp;
+  private int voltagem;
+  private String tamanho;
+  private float preço;
+  private boolean apagada;
+  private boolean meiaLuz;
+
+  // Método Getter
+
+  public String getTipoDaLamp() {
+    return tipoDaLamp;
+  }
+
+  public int getVoltagem() {
+    return voltagem;
+  }
+
+  public String getTamanho() {
+    return tamanho;
+  }
+
+  public float getPreço() {
+    return preço;
+  }
+
+  public boolean IsApagada() {
+    return apagada;
+  }
+
+  public boolean IsMeiaLuz() {
+    return meiaLuz;
+  }
+  
+  // Método Setter
+
+  public void setTipoDaLamp(String tipoDaLamp) {
+    this.tipoDaLamp = tipoDaLamp;
+  }
+
+  public void setVoltagem(int voltagem) {
+    this.voltagem = voltagem;
+  }
+
+  public void setTamanho(String tamanho) {
+    this.tamanho = tamanho;
+  }
+
+  public void setPreço(float preço) {
+    this.preço = preço;
+  }
+
+  public void setApagada(boolean apagada) {
+    this.apagada = apagada;
+  }
+
+  public void setMeiaLuz(boolean meiaLuz) {
+    this.meiaLuz = meiaLuz;
+  }
+  
+  // Métodos da classe
+
+  public void acender() {
+    if (apagada) {
+      setApagada(false);
+    }
+    else 
+      System.out.printf("\nErro! A Lâmpada já se encontra acesa");
+  } 
+
+  public void apagar() {
+    if (apagada == false) {
+      setApagada(true);
+      setMeiaLuz(false);
+    }
+    else
+      System.out.printf("\nErro! A Lâmpada já se encontra apagada");
+  }
+
+  public void deixarEmMeiaLuz() {
+    if (apagada)
+      System.out.printf("\nErro! A Lâmpada está apagada");
+    else {
+      if (meiaLuz == false) 
+        setMeiaLuz(true);
+      else
+        System.out.printf("\nErro! A Lâmpada já se encontra em meia-luz");
+    }
+  }
+
+  public void inicializarLampada(String tipoDaLamp, int volt, String tam, float preço, boolean apagada, boolean meiaLuz) {
+    setTipoDaLamp(tipoDaLamp);
+    setVoltagem(volt);
+    setTamanho(tam);
+    setPreço(preço);  
+    setApagada(apagada);
+    setMeiaLuz(meiaLuz);
+  }
+
+  private void mostrarEstado() {
+    if (apagada)
+      System.out.printf("\nA lâmpada está apagada");
+    else if (meiaLuz)
+      System.out.printf("\nA lâmpada está em meia-luz");
+    else 
+      System.out.printf("\nA lâmpada está acesa");
+  }
+
+  public void imprimirLampada() {
+    System.out.printf("\n--- Dados da Lâmpada ---\n");
+    System.out.printf("\nTipo: %s", tipoDaLamp);
+    System.out.printf("\nVoltagem: %dV", voltagem);
+    System.out.printf("\nTamanho: %s", tamanho);
+    System.out.printf("\nPreço: R$%.2f", preço);
+    mostrarEstado();
+  }
+}
