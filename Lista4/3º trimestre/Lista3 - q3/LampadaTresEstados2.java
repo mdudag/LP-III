290e25eae2237a08dde1f9a7bf2afd2d4ee2ad16
Duda <106630667+mdudag@users.noreply.github.com>
2023-04-26 22:47:05 -0300

    Create LampadaTresEstados2.java

---
+++ Lista4/3º trimestre/Lista3 - q3/LampadaTresEstados2.java
@@ -0,0 +1,170 @@
+import java.lang.Math;
+
+public class LampadaTresEstados2 {
+  private String tipoDaLamp;
+  private int voltagem;
+  private String tamanho;
+  private float preço;
+  private boolean apagada;
+  private boolean meiaLuz;
+  private int lumin;
+
+  // Método Getter
+
+  public String getTipoDaLamp() {
+    return tipoDaLamp;
+  }
+
+  public int getVoltagem() {
+    return voltagem;
+  }
+
+  public String getTamanho() {
+    return tamanho;
+  }
+
+  public float getPreço() {
+    return preço;
+  }
+
+  public boolean IsApagada() {
+    return apagada;
+  }
+
+  public boolean IsMeiaLuz() {
+    return meiaLuz;
+  }
+
+  public int getLumin() {
+    return lumin;
+  }
+  
+  // Método Setter
+
+  public void setTipoDaLamp(String tipoDaLamp) {
+    this.tipoDaLamp = tipoDaLamp;
+  }
+
+  public void setVoltagem(int voltagem) {
+    this.voltagem = voltagem;
+  }
+
+  public void setTamanho(String tamanho) {
+    this.tamanho = tamanho;
+  }
+
+  public void setPreço(float preço) {
+    this.preço = preço;
+  }
+
+  public void setApagada(boolean apagada) {
+    this.apagada = apagada;
+  }
+
+  public void setMeiaLuz(boolean meiaLuz) {
+    this.meiaLuz = meiaLuz;
+  }
+
+  public void setLumin(int lumin) {
+    this.lumin = lumin;
+  }
+  
+  // Métodos da classe
+
+  public void inicializarLampada(String tipoDaLamp, int volt, String tam, float preço, int lumin) {
+    setTipoDaLamp(tipoDaLamp);
+    setVoltagem(volt);
+    setTamanho(tam);
+    setPreço(preço);  
+    
+    // caso seja enviado um número maior que 100 ou negativo
+    int iluminacao = Math.abs(lumin%100);
+    
+    setLumin(iluminacao); 
+    mudarEstado(); 
+  }
+
+  public void acender() {
+    if (apagada) {
+      setApagada(false);
+    }
+    else 
+      System.out.printf("\nErro! A Lâmpada já se encontra acesa");
+  } 
+
+  public void apagar() {
+    if (apagada == false) {
+      setApagada(true);
+      setMeiaLuz(false);
+    }
+    else
+      System.out.printf("\nErro! A Lâmpada já se encontra apagada");
+  }
+
+  public void deixarEmMeiaLuz() {
+    if (apagada)
+      System.out.printf("\nErro! A Lâmpada está apagada");
+    else {
+      if (meiaLuz == false) 
+        setMeiaLuz(true);
+      else
+        System.out.printf("\nErro! A Lâmpada já se encontra em meia-luz");
+    }
+  }
+
+  public void mudarEstado() {
+    if (lumin > 0) { 
+      setApagada(false);
+
+      if (lumin != 100)
+        setMeiaLuz(true);
+      }
+    else if (lumin == 0)
+      setApagada(true);
+  }
+
+  public void aumentarLuz() {
+    if (lumin < 100)
+      ++lumin;
+    
+      if (apagada == true)
+        mudarEstado();
+    else
+      System.out.printf("\nA iluminação já está no máximo");
+  }
+
+  public void diminuirLuz() {
+    if (lumin > 0)
+      --lumin;
+    
+      if (apagada == false)
+        mudarEstado();
+    else
+      System.out.printf("\nA iluminação já está no mínimo");
+  }
+
+  public void definirLuz(int lumin) {
+    int iluminacao = Math.abs(lumin%100);
+    
+    setLumin(iluminacao);
+  }
+
+  private void mostrarEstado() {
+    if (apagada)
+      System.out.printf("\nA lâmpada está apagada");
+    else if (meiaLuz)
+      System.out.printf("\nA lâmpada está em meia-luz");
+    else 
+      System.out.printf("\nA lâmpada está acesa");
+  }
+
+  public void imprimirLampada() {
+    System.out.printf("\n--- Dados da Lâmpada ---\n");
+    System.out.printf("\nTipo: %s", tipoDaLamp);
+    System.out.printf("\nVoltagem: %dV", voltagem);
+    System.out.printf("\nTamanho: %s", tamanho);
+    System.out.printf("\nPreço: R$%.2f", preço);
+    System.out.printf("\nLuminosidade: %d", lumin);
+    mostrarEstado();
+  }
+}
