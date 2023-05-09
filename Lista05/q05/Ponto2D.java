cf448e3a8d61901a3706de1c357eeaf8ec4710bc
Duda <106630667+mdudag@users.noreply.github.com>
2023-05-09 01:24:55 -0300

    Create Ponto2D.java

---
+++ Lista05/q05/Ponto2D.java
@@ -0,0 +1,62 @@
+import java.lang.Math;
+
+public class Ponto2D {
+  private float x;
+  private float y;
+
+  // Método Getter
+
+  public Ponto2D(float x, float y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public float getX() {
+    return x;
+  }
+
+  public float getY() {
+    return y;
+  }
+  
+  // Métodos da classe
+
+  public boolean isEixoX() {
+    if ((y == 0) && (x != 0))
+      return true;
+    
+    return false;
+  }
+  
+  public boolean isEixoY() {
+    if ((x == 0) && (y != 0))
+      return true;
+    
+    return false;
+  }
+  
+  public boolean isEixos() {
+    if ((x == 0) && (y == 0))
+      return true;
+    
+    return false;
+  }
+  
+  public int quadrante() {
+    if (x > 0) 
+      return (y > 0) ? 1 : 4;
+    else if (x < 0)
+      return (y > 0) ? 2 : 3;
+    else
+      return 0;
+  }
+
+  public float distancia(Ponto2D p) {
+    return ((float) (Math.sqrt(Math.pow((p.x - this.x), 2) + Math.pow((p.y - this.y), 2))));
+  }
+
+  public void imprimirPonto() {
+    System.out.printf("\nPonto: (%.2f, %.2f)", x, y);
+    System.out.printf("\nO ponto se encontra no %dº quadrante", quadrante());
+  }
+}
