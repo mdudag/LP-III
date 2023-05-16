7daaae4cd1c4e90db61f9cf9a3bd90cafe65e8e9
Duda <106630667+mdudag@users.noreply.github.com>
2023-05-15 21:24:07 -0300

    Create CirculoExample.java

---
+++ Lista06/q5/CirculoExample.java
@@ -0,0 +1,13 @@
+public class CirculoExample {
+  public static void main(String args[]) {
+    Circulo circulo1 = new Circulo(2, 3, -18);
+    Circulo circulo2 = new Circulo();
+    Circulo circulo3 = new Circulo(10, -3);
+    Circulo circulo4 = new Circulo(4);
+    
+    circulo1.imprimirCirculo();
+    circulo2.imprimirCirculo();
+    circulo3.imprimirCirculo();
+    circulo4.imprimirCirculo();
+  }
+}
