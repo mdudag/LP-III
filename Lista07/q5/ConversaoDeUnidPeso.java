1714b5d8cb319353a60aa9984656200e97af74ba
Duda <106630667+mdudag@users.noreply.github.com>
2023-05-22 20:04:54 -0300

    Create ConversaoDeUnidPeso.java

---
+++ Lista07/q5/ConversaoDeUnidPeso.java
@@ -0,0 +1,49 @@
+public class ConversaoDeUnidPeso {
+  public static float KgParaG(float valor) {
+    return valor * 1000f;
+  }
+
+  public static float gParaKg(float valor) {
+    return valor / 1000f;
+  }
+
+  public static float KgParaLibra(float valor) {
+    return valor * 2.205f;
+  }
+
+  public static float LibraParaKg(float valor) {
+    return valor / 2.205f;
+  }
+
+  public static float KgParaOnça(float valor) {
+    return valor * 35f;
+  }
+
+  public static float OnçaParaKg(float valor) {
+    return valor / 35f;
+  }
+
+  public static float gParaLibra(float valor) {
+    return valor / 453.6f;
+  }
+
+  public static float LibraParaG(float valor) {
+    return valor * 453.6f;
+  }
+
+  public static float gParaOnça(float valor) {
+    return valor / 28.35f;
+  }
+
+  public static float OnçaParaG(float valor) {
+    return valor * 28.35f;
+  }
+
+  public static float LibraParaOnça(float valor) {
+    return valor * 16f;
+  }
+
+  public static float OnçaParaLibra(float valor) {
+    return valor / 16f;
+  }
+}
