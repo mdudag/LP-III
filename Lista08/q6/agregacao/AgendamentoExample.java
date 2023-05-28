c227cac6ac3277f1143c371e119c191a0de00222
Duda <106630667+mdudag@users.noreply.github.com>
2023-05-28 00:08:33 -0300

    Create AgendamentoExample.java

---
+++ Lista08/q6/agregacao/AgendamentoExample.java
@@ -0,0 +1,12 @@
+public class AgendamentoExample {
+  public static void main(String args[]) {
+    Data data = new Data(27, 5, 2023);
+    TimeAsInt time = new TimeAsInt(0,0,45);
+    Contato contato = new Contato("Maria", "maria@guedes.com", "73 912345678", 3, 11, 2002);
+    
+    Agendamento agend = new Agendamento(data, time, contato);
+
+    System.out.println(agend.getAgendamento());
+    Agendamento.prnNumAgend();
+  }
+}
