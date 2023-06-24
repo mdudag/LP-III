1b851c5c14fa63cbc016fbfe3cc4e1011ad3f85e
Duda <106630667+mdudag@users.noreply.github.com>
2023-06-24 19:17:42 -0300

    Create Pessoa.java

---
+++ Lista10/q03/Pessoa.java
@@ -0,0 +1,25 @@
+package q03;
+
+public class Pessoa {
+    private String nome;
+    private long id;
+    private Data nascimento;
+    protected final static double valorMinEmprestimo = 1000;
+
+    Pessoa(String n, int i, Data d) {
+        nome = n;
+        id = i;
+        nascimento = d;
+    }
+
+    @Override
+    public String toString() {
+        String resultado;
+        resultado = "Nome: "+nome+
+                    "\nIdentidade: "+id+"\n";
+        if (this.nascimento != null)
+            resultado += "Data de Nascimento: "+nascimento.dayToPrintLong(nascimento);
+                
+        return resultado;
+    }
+}
