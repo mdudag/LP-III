3beac84c31c9fee1bad3791181b7ea70029b5fd0
Duda <106630667+mdudag@users.noreply.github.com>
2023-06-24 15:19:26 -0300

    Create ChefeDeDepartamento.java

---
+++ Lista11/q04/ChefeDeDepartamento.java
@@ -0,0 +1,29 @@
+package q04;
+
+public class ChefeDeDepartamento extends Funcionario{
+    private String departamento;
+    private Data promocaoChefe;
+
+    ChefeDeDepartamento(String nome, int id, Data nascm, Data adm, 
+                        double sal, String dep, Data prom) {
+        super(nome, id, nascm, adm, sal);
+        departamento = dep;
+        promocaoChefe = prom;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ 
+               "\nDepartamento: "+departamento+"\n"+
+               "Data de promoçao ao cargo: "+promocaoChefe.dayToPrintLong(promocaoChefe);
+    }
+
+    public String getDepartamento(){
+        return departamento;
+    }
+
+    @Override
+    public double calculaEmprestimo() {
+        return super.qualSalario()*3;
+    }
+}
