d10bd6d29328115a96934840e31b99898065eab3
Duda <106630667+mdudag@users.noreply.github.com>
2023-06-24 19:13:39 -0300

    Create Funcionario.java

---
+++ Lista10/q02/Funcionario.java
@@ -0,0 +1,32 @@
+package q02;
+
+public class Funcionario extends Pessoa {
+    private Data admissao;
+    private double salario;
+
+    Funcionario(String nome, int id, Data nascm, Data adm, double sal) {
+        super(nome, id, nascm);
+        admissao = adm;
+        salario = sal;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+
+               "\nData de admissao: "+admissao.dayToPrintLong(admissao)+
+               "\nSalário: R$ "+salario;
+    }
+
+    final public double qualSalario(){
+        return salario;
+    }
+
+    public double calculaEmprestimo() {
+        double aux = salario * 75/100;
+
+        if (aux < 1000)
+            return valorMinEmprestimo;
+        
+        return aux;
+    }
+}
