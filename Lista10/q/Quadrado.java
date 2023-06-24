76376b62e5fb4987678389a59b3d54ebf05590a5
Duda <106630667+mdudag@users.noreply.github.com>
2023-06-24 00:02:06 -0300

    Create Quadrado.java

---
+++ Lista10/q/Quadrado.java
@@ -0,0 +1,32 @@
+package q;
+
+public class Quadrado extends Shape2D {
+    public Quadrado(float x, float y, float r) {
+        super(x, y, r);
+    }
+
+    public Quadrado(Quadrado quad) {
+        super(quad.p.x, quad.p.y, quad.reta);
+    }
+
+    public Quadrado() {
+        super(0, 0, 1);
+    }
+
+    // Outros Métodos
+
+    @Override
+    public String toString() {
+        return "\n  --- Quadrado ---\n" +
+               p.toString() +
+               "\nLado: " + reta;
+    }
+
+    public float calculaArea() {
+        return reta*reta;
+    }
+
+    public float calculaPerimetro() {
+        return 4*reta;
+    }
+}
