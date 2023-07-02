ea6434c4004590c937f90ac5e0d366cb545673f7
Duda <106630667+mdudag@users.noreply.github.com>
2023-07-01 21:46:15 -0300

    Update PrintTabuleiro.java

--- Projeto Final/projeto/PrintTabuleiro.java
+++ Projeto Final/projeto/PrintTabuleiro.java
@@ -2,13 +2,14 @@ package projeto;
 
 public class PrintTabuleiro {
     private String print[];
+    private String stringAnterior;
 
     public PrintTabuleiro() {
         // "o": Local que não foi percorrido
         // "#": Local que foi percorrido ou onde o jogador se encontra
-        // "@": Local onde há uma surpresa;
+        // "@": Local onde há uma surpresa
 
-        String print[] = {"                 o",                             // nível 0
+        String print[] = {"                 #",                             // nível 0
                           "                / \\", 
                           "               o   o",                           // nível 1
                           "              / \\ / \\",
@@ -27,23 +28,38 @@ public class PrintTabuleiro {
                           " o   o   o   o   o   o   o   o   o\n"};          // nível 8
 
         this.print = print;
+        stringAnterior = "";
     }
 
     public void printTabuleiro(int nivel, int direcao) {
+        // Para retirar a setinha que aponta ao nível anterior neste momento
+        if (nivel > 0 && stringAnterior != "")
+            print[2*(nivel-1)] = stringAnterior;
+
         //System.out.println("teste");
         switch(nivel) {
-            case -1:                                    break; // Para impressão sem modificações no tabuleiro
-            case 0: print[0] = modificaNivel0();        break;
-            case 1: print[1] = modificaNivel1(direcao); break;
-            case 2: print[2] = modificaNivel2(direcao); break;
-            case 3: print[3] = modificaNivel3(direcao); break;
-            case 4: print[4] = modificaNivel4(direcao); break;
-            case 5: print[5] = modificaNivel5(direcao); break;
-            case 6: print[6] = modificaNivel6(direcao); break;
-            case 7: print[7] = modificaNivel7(direcao); break;
-            case 8: print[8] = modificaNivel8(direcao); break;
+            case 0:                                      break; // Apenas mostra o nível 0
+            case 1: print[2] = modificaNivel1(direcao);  break;
+            case 2: print[4] = modificaNivel2(direcao);  break;
+            case 3: print[6] = modificaNivel3(direcao);  break;
+            case 4: print[8] = modificaNivel4(direcao);  break;
+            case 5: print[10] = modificaNivel5(direcao); break;
+            case 6: print[12] = modificaNivel6(direcao); break;
+            case 7: print[14] = modificaNivel7(direcao); break;
+            case 8: print[16] = modificaNivel8(direcao); break;
+            default: 
+                System.out.println("\nErro! Esse nível não existe."); return;
         }
         //System.out.println("teste2");
+
+        // Acrescenta uma setinha para identificar o nível no tabuleiro
+        //if (nivel != 0) 
+            stringAnterior = print[2*nivel];
+        
+            print[2*nivel] += "  <-";
+
+            if (nivel == 8) print[16] += "\n"; 
+        
 
         for (String i: print)
             System.out.println(i);
@@ -51,9 +67,13 @@ public class PrintTabuleiro {
         //System.out.println("teste3");
     }
 
-    private String modificaNivel0() {
-        return "                 #";
+    public void printTabuleiro() {  // revisar
+        printTabuleiro(0, 0);
     }
+
+    // private String modificaNivel0() {
+    //     return "                 #";
+    // }
 
     private String modificaNivel1(int direcao) {
         switch(direcao) {
@@ -127,15 +147,15 @@ public class PrintTabuleiro {
 
     private String modificaNivel8(int direcao) {
         switch(direcao) {
-            case 0: return " #   o   o   o   o   o   o   o   o\n";
-            case 1: return " o   #   o   o   o   o   o   o   o\n";
-            case 2: return " o   o   #   o   o   o   o   o   o\n";
-            case 3: return " o   o   o   @   o   o   o   o   o\n";
-            case 4: return " o   o   o   o   #   o   o   o   o\n";
-            case 5: return " o   o   o   o   o   #   o   o   o\n";
-            case 6: return " o   o   o   o   o   o   #   o   o\n";
-            case 7: return " o   o   o   o   o   o   o   @   o\n";
-            default: return " o   o   o   o   o   o   o   o   #\n";
+            case 0: return " #   o   o   o   o   o   o   o   o";
+            case 1: return " o   #   o   o   o   o   o   o   o";
+            case 2: return " o   o   #   o   o   o   o   o   o";
+            case 3: return " o   o   o   @   o   o   o   o   o";
+            case 4: return " o   o   o   o   #   o   o   o   o";
+            case 5: return " o   o   o   o   o   #   o   o   o";
+            case 6: return " o   o   o   o   o   o   #   o   o";
+            case 7: return " o   o   o   o   o   o   o   @   o";
+            default: return " o   o   o   o   o   o   o   o   #";
         }
     }
 }
