ec9a282ef52fa6709700798aca91d9004e4e3d15
Duda <106630667+mdudag@users.noreply.github.com>
2023-07-01 21:43:37 -0300

    Update Local.java

--- Projeto Final/projeto/Local.java
+++ Projeto Final/projeto/Local.java
@@ -3,18 +3,10 @@ package projeto;
 import java.util.Scanner;
 import java.lang.Math;
 
-public class Local {   // Classe de serviços ou utilitária: apresenta apenas métodos
+public class Local extends LagoNoCaminho {   // Classe de serviços ou utilitária: apresenta apenas métodos
     // quando passar por um caminho, limpar console
-
+    static Scanner ler = new Scanner(System.in);
     // Outros Métodos
-
-    static public void systemPause() {
-        Scanner ler = new Scanner(System.in);
-
-        System.out.println("\nPressione enter...");
-        String resp = ler.nextLine();
-        return;
-    }
 
     public int ganhaQuatroMoedas(String jogador) {
         System.out.printf("\n+-------  %s ganhou 4 moedas!  -------+\n", jogador);
@@ -22,10 +14,10 @@ public class Local {   // Classe de serviços ou utilitária: apresenta apenas m
     }
 
     public void localVazio() {
-        System.out.println("\nLocal vazio, siga em frente.");
+        System.out.println("\n === Local vazio, siga em frente. ===");
     }
 
-    public int supresa(int num, String jogador, int moedas, int moedasAdv) {
+    public int surpresa(int num, String jogador, int moedas, int moedasAdv) {
         switch(num) {
         case 0:
             System.out.printf("\n+-------  %s ganhou o dobro de moedas!  -------+\n", jogador);
@@ -37,7 +29,6 @@ public class Local {   // Classe de serviços ou utilitária: apresenta apenas m
             System.out.printf("\n+-------  %s ganhou a quantidade do adversário!  -------+\n", jogador);
             return moedas + moedasAdv;
         case 3:
-            Scanner ler = new Scanner(System.in);
 
             System.out.println("\nSem saber quantas moedas seu adversário possui, você pode escolher"+
                                "\nse deseja trocar as suas moedas pelas dele ou não." + 
@@ -49,44 +40,67 @@ public class Local {   // Classe de serviços ou utilitária: apresenta apenas m
             else return moedas;
             
         case 4:
-            System.out.printf("\n+-------  %s perdeu todas as moedas. Siga em frente para recuperá-las  -------+\n", jogador);
+            System.out.printf("\n+-------  %s perdeu todas as moedas.  -------+\n", jogador);
+
+            if (JogoTabuleiro.nivelAtual != 8)
+                System.out.println(" Siga em frente para recuperá-las.");
+
             return 0;
         default:
-            System.out.printf("\n+-------  %s perdeu metade das suas moedas. Ainda não acabou, continue em frente  -------+\n", jogador);
+            System.out.printf("\n+-------  %s perdeu metade das suas moedas.  -------+\n", jogador);
+
+            if (JogoTabuleiro.nivelAtual != 8)
+                System.out.println(" Ainda não acabou, continue em frente."); 
+
             return moedas/2;
         }
     }
 
     public void lagoNoCaminho(int num) {
+        desenhoDoLago();
+        int resp;
+
         switch(num) {
         case 0:
-            Scanner ler = new Scanner(System.in);
+            System.out.println("\nHá um lago no caminho, você precisa atravessá-lo.");
+            MetodoStatic.systemPause();
+            System.out.println("Você tem duas opções:\n"+
+                               "  1 - Dar a volta no lago\n"+
+                               "  2 - Pedir ajuda"); 
+            resp = Local.ler.nextInt(); 
 
-            System.out.println("\nHá um lago no caminho, você precisa atravessá-lo." + 
-                               "\nPeça ajuda para chegar do outro lado, lá tem algo para você!");
-            systemPause();
-            System.out.println("Deseja pedir ajuda?\n  1 - Pedir ajuda\n  2 - Perder algumas moedas para escapar"); 
-            int resp = ler.nextInt(); 
-
-            if (resp == 1) System.out.println("Pediu ajuda e passou");
-            else System.out.println("Perdeu moedas");
+            if (resp == 1) darVoltaNoLago();
+            else           pedirAjudaNoLago();
             break;
         case 1:
-            System.out.println("Você caiu no lago! Melhor pedir ajuda.");
+            System.out.println("\nNossa! Você caiu em um lago, precisa atravessá-lo.");
+            MetodoStatic.systemPause();
+            System.out.println("Você tem duas opções:\n"+
+                               "  1 - Ir nadando\n"+
+                               "  2 - Pedir ajuda"); 
+            resp = Local.ler.nextInt(); 
+
+            if (resp == 1) irNadando();
+            else           pedirAjudaNoLago();
             break;
         }
+        
+        MetodoStatic.systemPause();
     }
 
-    public void suspeitoNoCaminho(int num) {
-        System.out.println("Tem alguém à frente. Parece ferido. Deseja ajudar?");
+    public void desconhecidoNoCaminho(int num) {
+        Desconhecido pessoa;  // Polimorfismo de subclasse
 
         switch(num) {
-        case 0:
-            System.out.println("Era um ladrão.");
-            break;
-        case 1:
-            System.out.println("Era uma pessoa ferida.");
-            break;
+            case 0: pessoa = new IndigenaFerido(); break;
+            default: pessoa = new AventureiroFerido(); break;
         }
+
+        System.out.println("Tem alguém à frente...");
+        pessoa.desenhoDesconhecido();
+
+        MetodoStatic.systemPause();
+        System.out.print("Acho que está ferido. ");
+        pessoa.pedirAjuda();
     }
 }
