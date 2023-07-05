6fc0be47680049702bf127410c3f80ddf032e8c1
Duda <106630667+mdudag@users.noreply.github.com>
2023-07-05 07:01:00 -0300

    Update Jogo.java

--- Projeto Final/projeto/Jogo.java
+++ Projeto Final/projeto/Jogo.java
@@ -4,7 +4,5 @@ public interface Jogo {
     void iniciarJogo();
     void contextoDoJogo();
     void informacoesDoJogo();
-    void mostrarTabuleiroDoJogo(int jogador, int direcao);
-    int agitarAmuleto(Tabuleiro tab);
     void fimDoJogo();
 }
